a6822eb5ce1fffc5ab6cb6c0952c8b1818db29bc
Watch-dog-s <abduhanov-m@mail.ru>
2025-05-20 15:14:21 +0300

    Добавил экраны

---
+++ mobile/app/src/main/java/com/example/mobile/screens/Notification.kt
@@ -0,0 +1,10 @@
+package com.example.mobile.screens
+
+
+enum class A { Good, Norm, Bad }  // это надо для домашнего экрана там в зависимости от значения будут разные иконки типо получил 2 - красная 5 - зелёная
+enum class B { Visit, Mark, Report }
+
+data class Notification (
+    val a:A,
+    val b:B
+)
--- mobile/app/src/main/java/com/example/mobile/screens/Shedule_Screen.kt
+++ mobile/app/src/main/java/com/example/mobile/screens/Shedule_Screen.kt
@@ -1,8 +1,19 @@
 package com.example.mobile.screens
 
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 
@@ -15,5 +26,45 @@ fun Shedule_Screen_Prev(){
 
 @Composable
 fun Shedule_Screen(navController: NavHostController){
-    Text(text = "Shedule")
+    Column (modifier = Modifier.fillMaxSize()) {
+        //для примера
+        Subject("Математика", "Г-207", 100, "Иванов Иван Иванович", "9:00-10:30")
+        Spacer(modifier = Modifier.padding(10.dp))
+        chillTime("10 минут")
+        Spacer(modifier = Modifier.padding(10.dp))
+        Subject("Математика", "Г-207", 100, "Иванов Иван Иванович", "9:00-10:30")
+
+    }
+}
+
+
+
+
+@Composable
+fun Subject(subject:String,   room:String,    mark:Int,   teacher:String    ,time:String){
+
+    Column(modifier = Modifier.fillMaxWidth().border(width = 2.dp, color = Color.Black).padding(4.dp)) {
+        Text(text=subject)
+        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween){
+            Text(text=room)
+
+            Text(text=mark.toString())
+        }
+        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween){
+            Text(text=teacher)
+            Text(text=time)
+        }
+    }
+
+
+}
+
+@Composable
+fun chillTime(time:String){
+    Column(modifier = Modifier.fillMaxWidth()) {
+        Text(text=time)
+
+    }
+
+
 }
